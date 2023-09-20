9918532669c5757e0f5ebffce1af7faa58d109b0
benwubing <benwubing@gmail.com>
2023-09-20 14:22:44 +0800

    remove swagger

--- routes/roles.js
+++ routes/roles.js
@@ -1,33 +1,6 @@
 const express = require("express");
 const router = express.Router();
 
-/**
- * @swagger
- * /projects/{projectId}/resources/{resourceId}/roles:
- *   get:
- *     summary: List Roles for a Resource
- *     description: Retrieve a list of roles associated with a specific GCP resource.
- *     parameters:
- *       - name: projectId
- *         in: path
- *         required: true
- *         description: The unique project ID.
- *         schema:
- *           type: string
- *       - name: resourceId
- *         in: path
- *         required: true
- *         description: The unique resource ID.
- *         schema:
- *           type: string
- *     responses:
- *       '200':
- *         description: Successful response
- *         content:
- *           application/json:
- *             schema:
- *               $ref: "#/components/schemas/RoleListResponse"
- */
 router.get("/", (req, res) => {
   const projectId = req.params.projectId;
   const resourceId = req.params.resourceId;
